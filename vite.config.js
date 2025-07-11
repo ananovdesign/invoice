a08b82015d202816289ee1f25068f3f659cb382b
ananovdesign <ananov.krasimir@gmail.com>
2025-07-11 15:34:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; // Import path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: { // Add this block
+    alias: {
+      // This alias tells Vite that when it sees 'Firebase', it should look in 'src/Firebase.js'
+      'Firebase': path.resolve(__dirname, 'src/Firebase.js'),
+    },
+  },
+  // ... other existing configurations ...
 })
